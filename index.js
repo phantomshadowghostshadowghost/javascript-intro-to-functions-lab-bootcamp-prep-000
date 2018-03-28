91c8c231c7a45506e69f45e0fbbaecb2ae1825e8
Lamido Tijjani <kinglamido@gmail.com>
2018-03-28 14:59:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,4 +13,14 @@ function logWhisper(string) {console.log(string.toLowerCase())
 
 
 function sayHiToGrandma(string) {
- if ()
+var lowercase = string
+var uppercase = string
+ var showLove = "I love you, Grandma."
+ if (uppercase.toUpperCase() === uppercase)
+ return("YES INDEED!")
+ else if (lowercase.toLowerCase() === lowercase)
+ return("I can't hear you!")
+ else if (showLove === string)
+ return("I love you, too.")
+}
+sayHiToGrandma("I love you, Grandma.")
